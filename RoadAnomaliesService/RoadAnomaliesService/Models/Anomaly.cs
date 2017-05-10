5a46bf47e4c3e992803f0fcb80ec8e6b3898de1a
Bartolomeo Lombardi <bartolombardi@gmail.com>
2017-05-10 16:20:32 +0200

    Class base

---
+++ RoadAnomaliesService/RoadAnomaliesService/Models/Anomaly.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RoadAnomaliesService.Models
+{
+    public class Anomaly
+    {
+        private float latitute;
+        private float longitude;
+        private bool flagAnomaly;
+        private float zReading;
+        private float trust;
+        private int count;
+
+        public float Latitute
+        {
+            get
+            {
+                return latitute;
+            }
+
+            set
+            {
+                latitute = value;
+            }
+        }
+
+        public float Longitude
+        {
+            get
+            {
+                return longitude;
+            }
+
+            set
+            {
+                longitude = value;
+            }
+        }
+
+        public bool FlagAnomaly
+        {
+            get
+            {
+                return flagAnomaly;
+            }
+
+            set
+            {
+                flagAnomaly = value;
+            }
+        }
+
+        public float ZReading
+        {
+            get
+            {
+                return zReading;
+            }
+
+            set
+            {
+                zReading = value;
+            }
+        }
+
+        public float Trust
+        {
+            get
+            {
+                return trust;
+            }
+
+            set
+            {
+                trust = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+
+            set
+            {
+                count = value;
+            }
+        }
+
+        public Anomaly() { }
+
+        public Anomaly(float latitute, float longitude, bool flagAnomaly, float zReading, float trust, int count)
+        {
+            this.latitute = latitute;
+            this.longitude = longitude;
+            this.flagAnomaly = flagAnomaly;
+            this.zReading = zReading;
+            this.trust = trust;
+            this.count = count;
+        }
+
+    }
+}
---
+++ RoadAnomaliesService/RoadAnomaliesService/Models/DataController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RoadAnomaliesService.Models
+{
+    public class DataController
+    {
+        public void addAnomaly(float latitude, float longitude, float zReading) { }
+        public List<Anomaly> getAllAnomaly()
+        {
+            List<Anomaly> result = new List<Anomaly>();
+            return result;
+        }
+    }
+}
